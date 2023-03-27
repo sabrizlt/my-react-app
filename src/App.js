c81f3a163cb0a13068c3261129e543b7520c25b5
sabrizlt <sabrizaltini6@gmail.com>
2023-03-27 16:29:03 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
+import ImageComponent from './ImageComponent';
+import ButtonComponent from './ButtonComponent';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ButtonComponent buttonText="FORZA NAPOLI" />
+        <ImageComponent src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyCudOGLSDtdliGOJaWAVZCv2VgrL8CgPKxg&usqp=CAU" alt="napoli" />
       </header>
     </div>
   );
---
+++ src/ImageComponent.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class ImageComponent extends Component {
+  render() {
+    return (
+      <img src={this.props.src} alt={this.props.alt} />
+    );
+  }
+}
+
+export default ImageComponent;
+
